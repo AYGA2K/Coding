7814ed6b9dfad3299e343aa18718ec2194b3e768
AYGA <garouatayoub@gmail.com>
2021-12-02 00:49:07 +0100

    Update

--- Web Devlopment/MiniProjet/index.js
+++ Web Devlopment/MiniProjet/index.js
@@ -2,7 +2,7 @@ $("a[data-toggle]").on("click", function (e) {
   e.preventDefault(); // prevent navigating
   var selector = $(this).data("toggle"); // get corresponding element
   $(".home-page").hide();
-  $(selector).show();
+  $(".booking-form-box").addClass("BG");
   $("#div2").hide();
   $(selector).show();
   $("#div3").hide();
@@ -11,4 +11,19 @@ $("a[data-toggle]").on("click", function (e) {
   $(selector).show();
   $("#div5").hide();
   $(selector).show();
+  $("#div6").hide();
+  $(selector).show();
+  $("#div7").hide();
+  $(selector).show();
+});
+$(".button-minimize").on("click", function () {
+  $(".left").toggle();
+});
+
+$("#img1").on("click", function () {
+  $(".home-page").show();
+  $("#div1").append(
+    '<iframe id="ifr" src="Morroco.html" frameborder="0"></iframe>'
+  );
+  $(".left").hide();
 });
